eb80f16369619f4596b019ebbe6db74032f0320e
zxaok <106792481+zxaok@users.noreply.github.com>
2024-07-02 16:56:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 const express = require('express');
 const axios = require('axios');
-const cheerio = require('cheerio');
 const NodeCache = require('node-cache');
 
 const app = express();
-const port = process.env.PORT || 3000;
-const cache = new NodeCache({ stdTTL: 600 }); // 缓存时间为10分钟
+const cache = new NodeCache({ stdTTL: 108000 }); // 缓存 30 小时，108000 秒
 
-app.get('/', async (req, res) => {
-  const cacheKey = 'videoUrl';
-  const cachedVideoUrl = cache.get(cacheKey);
+const targetUrl = 'http://cfyy.cc/cdn/ks/bilibili482946.flv';
 
-  if (cachedVideoUrl) {
-    return res.redirect(cachedVideoUrl);
-  }
-
+async function fetchVideoUrl() {
   try {
-    const url = 'https://cfyy.cc/api/ks/zb.php?id=CrossFire_2008';
-    const response = await axios.get(url, {
+    const response = await axios.get(targetUrl, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
       }
     });
 
-    const html = response.data;
-    const $ = cheerio.load(html);
+    const data = response.data;
+    const videoUrlMatch = data.match(/video\s*=\s*'([^']+)'/);
 
-    let videoUrl = '';
-    $('script').each((i, script) => {
-      const scriptContent = $(script).html();
-      if (scriptContent.includes('var ok=new Ckey')) {
-        const startIndex = scriptContent.indexOf("video:'") + 7;
-        const endIndex = scriptContent.indexOf("'", startIndex);
-        videoUrl = scriptContent.substring(startIndex, endIndex);
-      }
-    });
-
-    if (videoUrl) {
-      cache.set(cacheKey, videoUrl); // 缓存结果
-      res.redirect(videoUrl);
+    if (videoUrlMatch && videoUrlMatch[1]) {
+      return videoUrlMatch[1];
     } else {
-      res.send('未找到包含视频链接的脚本标签');
+      throw new Error('Video URL not found');
     }
   } catch (error) {
-    res.status(500).send('获取页面内容失败');
+    console.error('Error fetching video URL:', error);
+    throw error;
+  }
+}
+
+app.get('/', async (req, res) => {
+  const cachedVideoUrl = cache.get('videoUrl');
+
+  if (cachedVideoUrl) {
+    return res.send(`Cached video URL: ${cachedVideoUrl}`);
+  }
+
+  try {
+    const videoUrl = await fetchVideoUrl();
+    cache.set('videoUrl', videoUrl);
+    res.send(`Fetched video URL: ${videoUrl}`);
+  } catch (error) {
+    res.status(500).send('Error fetching video URL');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
